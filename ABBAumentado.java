cd1ee822fe1c99631552b32480c222be0fca356b
Sebas$ <sebaslopez2106@gmail.com>
2026-09-20 13:31:34 -0300

    eliminar method complete

--- ABBAumentado.java
+++ ABBAumentado.java
@@ -56,6 +56,7 @@ public class ABBAumentado <K extends Comparable<? super K>, V> {
 
             if ( nodo.izq == null ) {   // insercion directa
                 nodo.izq = new Nodo<K, V>( clave, valor );
+                nodo.tamano += 1;
                 return 1;   // retorna 1 para modificar los tamaños de sus ancestros +1
             }
 
@@ -68,6 +69,7 @@ public class ABBAumentado <K extends Comparable<? super K>, V> {
 
             if ( nodo.der == null ) { // insercion directa
                 nodo.der = new Nodo<K, V>( clave, valor );
+                nodo.tamano += 1;
                 return 1;   // retorna 1 para modificar los tamaños de sus ancestros +1
             }
 
@@ -84,26 +86,55 @@ public class ABBAumentado <K extends Comparable<? super K>, V> {
 
     // Eliminar >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     public V eliminar ( K clave ) throws ClaveInexistenteException {
+        if ( raiz == null ) { throw new ClaveInexistenteException("clave inexistente"); }
         if ( clave == null ) { throw new ClaveNulaException(); }
 
         this.visitas += 1;
         if ( raiz.clave.compareTo( clave ) == 0 ) {
+
             V data = raiz.valor;
-            if ( raiz.izq != null ) {
-                raiz = replace_deleted(raiz, raiz);
+
+            if ( raiz.izq != null && raiz.der == null ) {
+                raiz = raiz.izq; // la raiz pasa a ser su hijo izquierdo
+
+            } else if ( raiz.izq == null && raiz.der != null ) {
+                raiz = raiz.der; // la raiz pasa a ser su hijo derecho
+
+            } else if ( raiz.izq != null && raiz.der != null ) { // caso raiz con dos hijos
+
+                Nodo<K, V> arbol_izquierdo = raiz.izq;
+                Nodo<K, V> arbol_derecho = raiz.der;
+                raiz = extraer_sucesor( raiz.der, raiz.der );
+
+                raiz.izq = arbol_izquierdo; // vinculamos la rama izquierda al sucesor para no perderla
+
+                if (raiz != arbol_derecho) { // previene la autorreferencia
+                    raiz.der = arbol_derecho;
+                }
+
+            } else { // si la raiz es una hoja
+                raiz = null;
             }
 
-            raiz = raiz.der;
+            // recalcular el tamaño de la raiz
+            if ( raiz != null ) {
+                raiz.tamano = 1;
+                if ( raiz.izq != null ) { raiz.tamano += raiz.izq.tamano; }
+                if ( raiz.der != null ) { raiz.tamano += raiz.der.tamano; }
+            }
+
             return data;
         }
 
         this.visitas -= 1; // en eliminar_recursivo se volvera a agregar la vista a la raiz
         return eliminar_recursivo(clave, raiz, raiz, false);
-    }
+    } // <-> end elminimar method
 
 
 
     private V eliminar_recursivo ( K clave, Nodo<K, V> padre, Nodo<K, V> hijo, boolean der ) throws ClaveInexistenteException { // der (false -> izq)
+
+        this.visitas++;
 
         if ( hijo == null ) { throw new ClaveInexistenteException("clave inexistente"); }
 
@@ -111,65 +142,79 @@ public class ABBAumentado <K extends Comparable<? super K>, V> {
         if ( hijo.clave.compareTo(clave) == 0 ) { // se encuentra el nodo
 
             // comprobar si posee rama izquierda
-            if ( hijo.izq != null ) {
-                Nodo<K, V> replace = replace_deleted(hijo.izq, hijo.izq);
+            if ( hijo.izq != null && hijo.der == null ) {
+                if ( der ) { padre.der = hijo.izq; }
+                else { padre.izq = hijo.izq; }
+            }
 
-                if ( ! ( replace.clave.compareTo( hijo.izq.clave ) == 0 ) ) {   // si solo queda un nodo en la rama izq
+            else if ( hijo.izq == null && hijo.der != null ) {
+                if ( der ) { padre.der = hijo.der; }
+                else { padre.izq = hijo.der; }
+            }
 
-                    if ( der ) {
-                        padre.der = hijo.izq;
-                    } else { padre.izq = hijo.izq; }
+            else if ( hijo.izq != null && hijo.der != null ) {
+                Nodo<K, V> replace = extraer_sucesor(hijo.der, hijo.der);
 
-                } else {
-                    replace.izq = null;
-                }
-
-                replace.der = hijo.der;
-
+                // vincular replace al padre del eliminado
                 if ( der ) {
                     padre.der = replace;
-                } else {
-                    padre.izq = replace;
+                } else { padre.izq = replace; }
+
+                // salvar el subarbol derecho e izquierdo del eliminado
+                replace.izq = hijo.izq;
+                if (replace != hijo.der) { // previene la autoreferencia
+                    replace.der = hijo.der;
                 }
 
-                return hijo.valor;
+                // recalcular tamaño de replace
+                replace.tamano = 1;
+                if ( replace.izq != null ) { replace.tamano += replace.izq.tamano; }
+                if ( replace.der != null ) { replace.tamano += replace.der.tamano; }
 
-            // si no posee izquierda entonces se reemplaza directo por hijo.der
-            } else {
-                if ( der ) {
-                    padre.der = hijo.der;
-                } else {
-                    padre.izq = hijo.der;
-                }
             }
+
+            else { // ambas ramas son nulas
+                if ( der ) { padre.der = null; }
+                else { padre.izq = null; }
+            }
+
+            return hijo.valor;
         }
-        
+
         // 2. busqueda
-        if ( hijo.clave.compareTo(clave) < 0 ) { // menor ->
-            return eliminar_recursivo(clave, hijo, hijo.izq, true);
+        if ( hijo.clave.compareTo(clave) < 0 ) { // menor (->)
+            V temp_value = eliminar_recursivo(clave, hijo, hijo.der, true);
+            hijo.tamano -= 1;
+            return temp_value;
 
-        } else {
-            return eliminar_recursivo(clave, hijo, hijo.der, false);
+        } else { // mayor (<-)
+            V temp_value = eliminar_recursivo(clave, hijo, hijo.izq, false);
+            hijo.tamano -= 1;
+            return temp_value;
         }
 
-    }
+    } // <-> end eliminar_recursivo method
 
 
-
+ 
         // esta funcion la utiliza eliminar_recursivo
-    private Nodo<K, V> replace_deleted ( Nodo<K, V> padre, Nodo<K, V> hijo ) {
-        if ( hijo.der == null ) { // el mas grande no tiene subarbol derecho
+    private Nodo<K, V> extraer_sucesor ( Nodo<K, V> padre, Nodo<K, V> hijo ) {
+        this.visitas++;
+
+        if ( hijo.izq == null ) { // el mas pequeño no tiene subarbol izquierdo
 
             if ( hijo == padre ) {
                 return hijo;
             }
 
-            padre.der = hijo.izq;
+            padre.izq = hijo.der; // se vincula para salvar el hijo derecho antes de ser reemplazado
             return hijo;    // el retorno es el nodo que reemplazara al eliminado
         }
 
-        return replace_deleted (hijo, hijo.der);  // recorre en busca del mas grande
-    }
+        hijo.tamano -= 1;
+        return extraer_sucesor (hijo, hijo.izq);  // baja por la izquierda buscando el minimo
+
+    } // <-> end extraer_sucesor method
 
     // Eliminar <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
